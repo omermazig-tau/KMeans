881e1d3d94a3217651cf8e77979c1a27513b2c26
omermazig <omermazig@gmail.com>
2021-12-07 00:43:49 +0200

    Move tests into a folder

--- Python/tests/test_sanity.py
+++ Python/tests/test_sanity.py
@@ -1,24 +1,24 @@
 import math
+import os
 
 import pytest
 
-from Python.kmeans import read_date_from_file, get_centroids_from_data_points, DEFAULT_ITERATIONS_NUMBER
+from Python.kmeans import read_date_from_file, get_centroids_from_data_points, DEFAULT_ITERATIONS_NUMBER, EPSILON
 
-kuku = [
-    (1, 3, 600),
-    (2, 7, DEFAULT_ITERATIONS_NUMBER),
-    (3, 15, 300),
+test_scenarios = [
+    (i, 7, DEFAULT_ITERATIONS_NUMBER) for i in range(1, 21)
 ]
 
 
 def compare_data_points(data_point, other_data_point):
     for a, b in zip(data_point, other_data_point):
-        assert math.isclose(a, b, abs_tol=0.0001)
+        assert math.isclose(a, b, abs_tol=EPSILON)
 
 
-@pytest.mark.parametrize("i,k,iterations", kuku)
+@pytest.mark.parametrize("i,k,iterations", test_scenarios)
 def test_sanity(k, iterations, i):
-    data_points = read_date_from_file(f'input_{i}.txt')
+    filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), f'input_{i}.txt')
+    data_points = read_date_from_file(filepath)
     centroids = get_centroids_from_data_points(data_points, k, iterations)
     expected_centroids = read_date_from_file(f'output_{i}.txt')
     assert len(centroids) == len(expected_centroids)
