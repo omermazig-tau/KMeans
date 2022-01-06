cfb130814ed0d4fda846ad196307bb2de2bd13c6
omermazig <omermazig@gmail.com>
2022-01-06 13:19:04 +0200

    Add `test_sanity_original_examples` test

--- Python/tests/test_sanity.py
+++ Python/tests/test_sanity.py
@@ -26,16 +26,36 @@ def _get_centroids_from_python(data_points, initial_centroids, iterations, k, ep
     _get_centroids_from_c,
     _get_centroids_from_python
 ])
+@pytest.mark.parametrize("i,k,iterations", [(1, 3, 100), (2, 7, None), (3, 15, 300)])
+def test_sanity_original_examples(k, iterations, i, get_centroids_callback):
+    epsilon = DEFAULT_EPSILON
+    if iterations is None:
+        iterations = DEFAULT_ITERATIONS_NUMBER
+
+    base_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'kmeans_files')
+    input_filepath = os.path.join(base_folder, f'input_{i}_orig.txt')
+    output_filepath = os.path.join(base_folder, f'output_{i}_orig.txt')
+    get_and_assert_new_centroids(k, iterations, epsilon, get_centroids_callback, input_filepath, output_filepath)
+
+
+@pytest.mark.parametrize("get_centroids_callback", [
+    _get_centroids_from_c,
+    _get_centroids_from_python
+])
 @pytest.mark.parametrize("i,k,iterations", test_scenarios)
 def test_sanity(k, iterations, i, get_centroids_callback):
     epsilon = DEFAULT_EPSILON
 
     base_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'kmeans_files')
     input_filepath = os.path.join(base_folder, f'input_{i}.txt')
+    output_filepath = os.path.join(base_folder, f'output_{i}.txt')
+    get_and_assert_new_centroids(k, iterations, epsilon, get_centroids_callback, input_filepath, output_filepath)
+
+
+def get_and_assert_new_centroids(k, iterations, epsilon, get_centroids_callback, input_filepath, output_filepath):
     data_points = read_date_from_file(input_filepath)
     initial_centroids = data_points[:k]
     centroids = get_centroids_callback(data_points, initial_centroids, iterations, k, epsilon)
-    output_filepath = os.path.join(base_folder, f'output_{i}.txt')
     expected_centroids = read_date_from_file(output_filepath)
     assert len(centroids) == len(expected_centroids)
     for i, centroid in enumerate(centroids):
