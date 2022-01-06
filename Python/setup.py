b6bf0e0c0942f32511d5dcad33539a879632b414
omermazig <omermazig@gmail.com>
2022-01-06 16:29:35 +0200

    Change path so linux is supported

--- Python/setup.py
+++ Python/setup.py
@@ -9,4 +9,6 @@ In addition, a minimalist setup is shown.
 setup(name='mykmeanssp',
       version='1.0',
       description='kmeans c api for sp class',
-      ext_modules=[Extension('mykmeanssp', sources=[os.path.join('..', 'CPart', 'kmeansApi.c')])])
+      ext_modules=[Extension('mykmeanssp',
+                             sources=[os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+                                                   'CPart', 'kmeansApi.c')])])
