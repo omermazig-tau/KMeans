8624f5b2b8dcab42a8527ab08f0f1b2d842be790
omermazig <omermazig@gmail.com>
2022-04-19 14:21:04 +0300

    Add `spkmeans_api` to setup. Doesn't have the logic yet - just a copy of `kmeansApi.c`

--- Python/setup.py
+++ Python/setup.py
@@ -9,6 +9,9 @@ In addition, a minimalist setup is shown.
 setup(name='mykmeanssp',
       version='1.0',
       description='kmeans c api for sp class',
-      ext_modules=[Extension('mykmeanssp',
-                             sources=[os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-                                                   'CPart', 'kmeansApi.c')])])
+      ext_modules=[
+          Extension('mykmeanssp', sources=[os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+                                                        'CPart', 'kmeansApi.c')]),
+          Extension('spkmeans_api', sources=[os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+                                                          'CPart', 'spkmeansmodule.c')])
+      ])
