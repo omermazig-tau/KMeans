34c933a337f6d929a32bf40f18d93fe022c8fe93
omermazig <omermazig@gmail.com>
2022-04-21 20:32:21 +0300

    Add tests for `spk`

--- Python/tests/test_spkmeans_sanity.py
+++ Python/tests/test_spkmeans_sanity.py
@@ -4,7 +4,8 @@ import os
 import pytest
 
 from Python.kmeans import read_date_from_file
-from Python.spkmeans import preform_specific_goal
+from Python.spkmeans import preform_specific_goal, spk
+from Python.tests import test_kmeans_pp_sanity
 
 DEFAULT_EPSILON = 0.0001
 
@@ -24,6 +25,24 @@ def test_specific_goal_sanity(i, goal):
     input_filepath = os.path.join(input_folder, file_name)
     output_filepath = os.path.join(output_folder, file_name)
     get_and_assert_matrix_result(goal, input_filepath, output_filepath)
+
+
+@pytest.mark.parametrize("i", range(10))
+def test_spk_sanity(i):
+    k = 0
+    input_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "spkmeans_files")
+    output_folder = os.path.join(input_folder, 'outputs', 'py', 'spk')
+    file_name = f"spk_{i}.txt"
+    input_filepath = os.path.join(input_folder, file_name)
+    output_filepath = os.path.join(output_folder, file_name)
+
+    matrix = read_date_from_file(input_filepath)
+    rows = len(matrix)
+    cols = len(matrix[0])
+    flatten_matrix = tuple(itertools.chain.from_iterable(matrix))
+
+    centroids, initial_centroids_indexes = spk(flatten_matrix, rows, cols, k)
+    test_kmeans_pp_sanity.get_and_assert_new_centroids(centroids, initial_centroids_indexes, output_filepath, DEFAULT_EPSILON)
 
 
 def get_and_assert_matrix_result(goal, input_filepath, output_filepath):
